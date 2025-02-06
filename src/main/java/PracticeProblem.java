0d91336cd06fd770fb63df121d76a78e4d2e4508
pavarasan-karunainathan-ics4u1-02-25 <166336911+pavarasan-karunainathan-ics4u1-02-25@users.noreply.github.com>
2025-02-06 14:55:38 +0000

    Problems Complete

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class PracticeProblem {
-
+	static Scanner sc;
 	public static void main(String args[]) {
 		q1();
 		q2();
@@ -12,39 +12,54 @@ public class PracticeProblem {
 		q7();
 	}
 
-	public static void q1() {
-		//Write question 1 code here
-		
+	public static void q1(){
+		sc = new Scanner(System.in);
+		System.out.print("In: ");
+		System.out.println(sc.nextLine().charAt(0));
 	}
 
-	public static void q2() {
-		//Write question 2 code here
-		
+	public static void q2(){
+		sc = new Scanner(System.in);
+		System.out.print("In: ");
+		System.out.println(!sc.nextBoolean());
 	}
 
-	public static void q3() {
-		//Write question 3 code here
-		
+	public static void q3(){
+		sc = new Scanner(System.in);
+		System.out.print("In: ");
+		System.out.println(sc.nextInt() > 5);
 	}
 
-	public static void q4() {
-		//Write question 4 code here
-		
+	public static void q4(){
+		sc = new Scanner(System.in);
+		System.out.print("In: ");
+		double num = sc.nextDouble();
+		System.out.println(-2 <= num && num <= 2);
 	}
 
-	public static void q5() {
-		//Write question 5 code here
-		
+	public static void q5(){
+		sc = new Scanner(System.in);
+		System.out.print("In: ");
+		System.out.println(sc.nextLine().equals("Hello World"));
 	}
 
-	public static void q6() {
-		//Write question 6 code here
-		
+	public static void q6(){
+		sc = new Scanner(System.in);
+		System.out.print("In: ");
+		int num1 = sc.nextInt();
+		sc.nextLine();
+		System.out.print("In: ");
+		int num2 = sc.nextInt();
+		System.out.println(num1 <= num2);
 	}
 
-	public static void q7() {
-		//Write question 7 code here
-		
+	public static void q7(){
+		sc = new Scanner(System.in);
+		System.out.print("In: ");
+		double num1 = sc.nextDouble();
+		System.out.print("In: ");
+		double num2 = sc.nextDouble();
+		System.out.println(num1 > num2);
 	}
 
 }
